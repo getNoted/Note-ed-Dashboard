ccce0f896d05a0779807865b2a2c964a2029cac1
Ishan <56102033+ishanjain1502@users.noreply.github.com>
2022-04-22 20:44:11 +0530

    changing flder position

--- src/Components/dashboard/DashboardHome.jsx
+++ src/Components/dashboard/DashboardHome.jsx
@@ -1,15 +1,48 @@
-import React from 'react';
-import Folders from './Folders';
+import AddIcon from '@mui/icons-material/Add';
+import { ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import React, { useEffect } from 'react';
+import Folder from './Folder';
 import VideoHome from './videos/VideoHome';
-
 export default function VideoList() {
+
+    const [activeFolder, setActiveFolder] = ("");
+    const [folders, setFolders] = (['video1' , 'video2'])
+    const getVideos = async() => {
+      console.log("inside GETTING VIDEOS FROM ACTIVE FOLDER");
+    }
+    useEffect(() => {
+    getVideos()
+    }, [activeFolder])
+      
+    let foldername="Default";
+      let videos=["video1","video2"]
+
     return (
         <div>
           
            
             <main className='flex mt-16'>
                 <div className='absolute top-32 left-5 folders-component ml-8 mr-8 basis-1/6'>
-                    <Folders />
+                <div className='max-w-1/5' >
+                        <Folder foldername={foldername} videos={videos}></Folder>
+                        <ListItem disablePadding>
+                            <ListItemButton>
+                            <ListItemIcon>
+                                <AddIcon></AddIcon>
+                            </ListItemIcon>
+                            <ListItemText primary="Create Folder" />
+                            </ListItemButton>
+                        </ListItem>
+
+                        {/* {
+                        videos.map((video) => (
+                            <div>
+                                {video}
+                            </div>
+                        ))
+                        } */}
+
+                    </div>
                     <section className='w-52 h-auto bg-new-green' >                       
                     </section>
                 </div>
